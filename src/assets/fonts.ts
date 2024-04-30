328b70db65c0e751eae09080935e26f24ed6b7c9
pervej0 <mdparvez222khan@gmail.com>
2024-04-30 11:56:12 +0600

    login page designed

---
+++ src/assets/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Roboto } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"], weight: "500" });
+export const roboto = Roboto({ subsets: ["greek"], weight: "700" });
